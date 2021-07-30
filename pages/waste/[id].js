5c1d88b9494647df7b87d1471e2cdb49965fca3a
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2021-07-30 15:04:23 +0100

    deploying minor changes to

---
+++ pages/waste/[id].js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function List(props) {
+    return (
+        <div>
+            waste List
+        </div>
+    )
+}
+
+List.propTypes = {
+
+}
+
+export default List
+
